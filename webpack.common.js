68a441616986e21eaf97a58c20303123d3a663a5
pandjihm <pandjihm@yahoo.com>
2021-05-16 12:37:16 +0700

    Navigation - First Section (Progress)

--- webpack.common.js
+++ webpack.common.js
@@ -21,6 +21,14 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          }
+        ]
+      },
     ],
   },
   plugins: [
